5b72db23045a9c2ec84301057f9a1ddd152f1391
eprayoga <endangprayoga95@gmail.com>
2023-12-11 23:14:48 +0700

    Change config to js

---
+++ config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  development: {
+    username: 'postgres',
+    password: 'yoga95',
+    database: 'kenari_development',
+    host: '127.0.0.1',
+    dialect: 'postgres',
+  },
+  test: {
+    username: 'root',
+    password: null,
+    database: 'kenari_test',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+  production: {
+    username: 'root',
+    password: null,
+    database: 'kenari_production',
+    host: '127.0.0.1',
+    dialect: 'mysql',
+  },
+};
